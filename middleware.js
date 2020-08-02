47832a30a7b86620b2377421923c001b2f80b7c6
kushal-kamra <kushalkamra@gmail.com>
2020-08-02 20:30:18 +0530

    Middleware

--- middleware.js
+++ middleware.js
@@ -1,5 +1,25 @@
 const VALID_KEYS_PATH = __dirname + '/valid-keys.txt';
+fs = require('fs');
+
 
 module.exports = function (req, res, next) {
+    let apikey = req['x-api-key'];
+    let keyindex = -1;
 
+    fs.readFile(VALID_KEYS_PATH, 'utf8', function(err, data) {
+
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log(data);
+
+        keyindex = data.indexOf(apikey);
+    });
+
+    if (keyindex === -1) {
+        res.status(401);
+    }
+
+    next();
 };
